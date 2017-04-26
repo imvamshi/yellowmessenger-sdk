5c6bc46cac1b90563aac67a631e2d897e00f340e
kishore <kishore@yellowmessenger.com>
2017-04-26 14:47:02 +0530

    Added upload service into the

--- yellowmessenger-sdk/src/main/java/com/yellowmessenger/sdk/service/XMPPService.java
+++ yellowmessenger-sdk/src/main/java/com/yellowmessenger/sdk/service/XMPPService.java
@@ -197,7 +197,7 @@ public class XMPPService extends Service {
 
     StanzaListener packetListener = new StanzaListener() {
         @Override
-        public void processPacket(Stanza stanza) throws SmackException.NotConnectedException, InterruptedException {
+        public void processStanza(Stanza stanza) throws SmackException.NotConnectedException, InterruptedException {
             if (stanza instanceof Message) {
                 Message message = (Message) stanza;
                 Jid sender = message.getFrom();
@@ -217,7 +217,7 @@ public class XMPPService extends Service {
     StanzaListener pingPacketListener = new StanzaListener() {
 
         @Override
-        public void processPacket(Stanza stanza) throws SmackException.NotConnectedException, InterruptedException {
+        public void processStanza(Stanza stanza) throws SmackException.NotConnectedException, InterruptedException {
             try{
                 mConnection.sendStanza(((Ping) stanza).getPong());
             }catch (Exception e){
@@ -356,6 +356,9 @@ public class XMPPService extends Service {
         @Override
         protected Void doInBackground(String... params) {
             Log.d(TAG, "Logging in...");
+            if(mConnection!=null && reconnect && mConnection.isConnected()){
+                mConnection.disconnect();
+            }
             XMPPService.this.login();
             return null;
         }
@@ -400,7 +403,7 @@ public class XMPPService extends Service {
             mConnection.addStanzaAcknowledgedListener(new StanzaListener(){
 
                 @Override
-                public void processPacket(Stanza packet) throws SmackException.NotConnectedException {
+                public void processStanza(Stanza packet) throws SmackException.NotConnectedException {
                     // TODO Acknowledgement
                     ChatMessage chatMessage = ChatMessageDAO.getChatMessageByStanzaId(packet.getStanzaId());
                     if(chatMessage!=null){
@@ -662,7 +665,7 @@ public class XMPPService extends Service {
         @Override
         protected void onPostExecute(Map map) {
             if(!(Boolean)map.get("unsent")){
-                new Handler().postDelayed(new AckRunnable((String)map.get("stanzaId")),5000);
+                new Handler().postDelayed(new AckRunnable((String)map.get("stanzaId")),8000);
             }
         }
     }
